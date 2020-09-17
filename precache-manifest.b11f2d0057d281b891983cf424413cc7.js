2c5217aad79b6e19da5dab730877c032f6306674
Evgeniy Panin <pane.ewgen@yandex.ru>
2020-09-17 19:58:33 +0400

    Updates

---
+++ precache-manifest.b11f2d0057d281b891983cf424413cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a795d04532c48cd42aa9550e540c5d",
+    "url": "/profilance-group-test/index.html"
+  },
+  {
+    "revision": "356e2de9feabf1e50e27",
+    "url": "/profilance-group-test/static/css/main.968cafb5.chunk.css"
+  },
+  {
+    "revision": "6b63814a7d09e433e689",
+    "url": "/profilance-group-test/static/js/2.201164f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/profilance-group-test/static/js/2.201164f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "356e2de9feabf1e50e27",
+    "url": "/profilance-group-test/static/js/main.cad67e9c.chunk.js"
+  },
+  {
+    "revision": "dcacc2caebb4bb5c8c5e",
+    "url": "/profilance-group-test/static/js/runtime-main.8035eee4.js"
+  }
+]);
